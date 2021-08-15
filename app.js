825f86d257b226481a0c2e58a13db403cff1fd18
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-15 14:52:57 -0230

    Testing the Tree nodes

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ class AVLTreeNode {
 	constructor(value, left = null, right = null) {
 		this.value = value;
 		this.left = left;
-		thid.right = right;
+		this.right = right;
 	}
 }
+
+let n = new AVLTreeNode(100);
+let n2 = new AVLTreeNode(200, n);
+
+console.log(n);
+console.log(n2);
