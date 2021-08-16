6e6f43e77011de84696888da710d0eb9c4daab02
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-16 12:46:17 -0230

    Set up express, set up nunjucks, routes

--- app.js
+++ app.js
@@ -1,106 +1,30 @@
 const treeify = require('treeify');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const nunjucks = require('nunjucks')
 
-class AVLTreeNode {
-	constructor(value, left = null, right = null) {
-		this.value = value;
-		this.left = left;
-		this.right = right;
-	}
-}
-class AVLTree {
-	constructor(root = null) {
-		this.root = root;
-	}
-	insert(value){
-		const recursiveHelper = (node) => {
-			if(node === null) {
-				return new AVLTreeNode(value);
-			}else if(value < node.value) {
-				node.left = recursiveHelper(node.left);
-			}else if(value > node.value) {
-				node.right = recursiveHelper(node.right);
-			}else{
-				throw new Error("Inserts into our AVLTree cannot be equal!")
-			}
-			
-			if(nodeBalance(node) > 1) {
-				return nodeRotateLeft(node);
-			}else if(nodeBalance < -1) {
-				return nodeRotateRight(node);
-			}else{
-				return node;
-			}
-		}
-		this.root = recursiveHelper(this.root);
-	}
-	search(value){
-		const recursiveSearchHelper = (node) => {
-			if(node === null) {
-				return false;
-			}else if(value < node.value){
-				return recursiveHelper(node.left);
-			}else if (value > node.value){
-				return recursiveHelper(node.right);
-			}else{
-				return true;
-			}
-		}
-		return recursiveSearchHelper(this.root)
-	}
-}
+const port = 3000
 
-const myTree = new AVLTree();
+//app.get('/', function (req, res) {
+//	res.render('app.html')
+//
+//});
 
-function nodeHeight(node){
-	if(node === null) {
-		return -1;
-	}else if(node.left === null && node.right === null){
-		return 0;
-	}else{
-		return 1 + Math.max(nodeHeight(node.left), nodeHeight(node.right));
-	}
-	
+const indexRouter = require('./routes/index');
+const submitRouter = require('./routes/submit');
 
-}
+nunjucks.configure('views', {
+	noCache: true,
+	autoescape: true,
+	express: app
+})
 
-function nodeBalance(node){
-	return nodeHeight(node.right) - nodeHeight(node.left);
-}
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', indexRouter);
+app.use('/submit', submitRouter);
 
-function nodeRotateLeft(node){
-	if(node === null || node.right === null){
-		return node;
-	}
-	const newRoot = node.right;
-	node.right = newRoot.left;
-	newRoot.left = node;
-	return newRoot;
-}
-
-function nodeRotateRight(node){
-	if(node === null || node.left === null){
-		return node;
-	}
-	const newRoot = node.left;
-	node.left = newRoot.right;
-	newRoot.right = node;
-	return newRoot;
-}
-
-
-
-myTree.insert(1);
-myTree.insert(2);
-myTree.insert(3);
-myTree.insert(4);
-myTree.insert(5);
-myTree.insert(6);
-myTree.insert(7);
-myTree.insert(8);
-myTree.insert(9);
-myTree.insert(10);
-
-console.log(treeify.asTree(myTree, true));
-console.log(JSON.stringify(myTree, null, 2));
-console.log(nodeHeight(myTree.root));
-console.log(nodeBalance(myTree.root));
+app.listen(port, () => {
+	console.log(`Listening on http://localhost:${port}`);
+});
