21e2674d1c9a7174d44ebefbf7aece63ac236faf
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-18 20:31:36 -0230

    First commit for the unit tests file

---
+++ __tests__/unit/tests.js
@@ -0,0 +1,20 @@
+const { insertIntoTree } = require('../../routes/submit')
+
+
+describe("Insert Test", () => {
+	it('This test should show that the insert function is operating succsesfully', () => {
+		expect.insertIntoTree()
+	})
+})
+
+
+
+
+
+
+
+//describe('Sample Test', () => {
+//	it('should test that true === true', () => {
+//	  expect(true).to//Be(true)
+//	})
+//  })
