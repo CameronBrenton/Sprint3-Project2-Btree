817d692d66d3021f5eee46228ded6e03d41aeb0b
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-24 15:33:56 -0230

    removed unused requirements and comments

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const treeify = require('treeify');
 const express = require('express');
 const app = express();
-const router = express.Router();
 const nunjucks = require('nunjucks')
 
 const port = 3000
 
-//app.get('/', function (req, res) {
-//	res.render('app.html')
-//
-//});
 
 const indexRouter = require('./routes/index');
 const submitRouter = require('./routes/submit');
